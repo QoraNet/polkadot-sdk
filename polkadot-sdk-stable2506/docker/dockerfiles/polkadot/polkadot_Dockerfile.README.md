17f17e4baa31a32b308daa9d7aa18429fbde0d47
Block Man DEV <194144408+blockmandev@users.noreply.github.com>
2025-10-26 00:12:55 -0600

    Delete polkadot-sdk-stable2506 directory

--- polkadot-sdk-stable2506/docker/dockerfiles/polkadot/polkadot_Dockerfile.README.md
+++
@@ -1,7 +0,0 @@
-# Polkadot official Docker image
-
-## [Polkadot](https://polkadot.com/)
-
-## [GitHub](https://github.com/paritytech/polkadot)
-
-## [Polkadot Wiki](https://wiki.polkadot.network/)
--- polkadot-sdk-stable2506/polkadot/zombienet-sdk-tests/tests/functional/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (C) Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
-
-mod approval_voting_coalescing;
-mod approved_peer_mixed_validators;
-mod async_backing_6_seconds_rate;
-mod duplicate_collations;
-mod spam_statement_distribution_requests;
-mod sync_backing;
